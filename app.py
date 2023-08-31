da867dbeabed3345313458106f7e2cebed0bef73
David Siles <89803426+siles4me@users.noreply.github.com>
2023-08-30 23:32:58 -0600

    actualizacion

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from audiorecorder import audiorecorder
+from audio_recorder_streamlit import audio_recorder
 import numpy as np
 from scipy.io import wavfile
 import scipy.fftpack as fourier
